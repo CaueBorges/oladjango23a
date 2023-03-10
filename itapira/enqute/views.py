69e4bc6422a69eb3b2bedb48bf5a28724c549985
CaueBo <samuelreiscosta515@gmail.com>
2023-03-10 08:50:34 -0300

    Configuração de língua e fusário

--- itapira/enqute/views.py
+++ itapira/enqute/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<h1>Olha, se voce nao me ama.<h1><h2>Caneta azul, azul caneta<h2>")
 
 # Create your views here.
