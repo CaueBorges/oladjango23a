dd141f50b5516e2cbec9e10ffe7038fcf593bf2a
CaueBo <samuelreiscosta515@gmail.com>
2023-03-10 08:51:47 -0300

    Criado a função

--- itapira/enqute/views.py
+++ itapira/enqute/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Olha, se voce nao me ama.<h1><h2>Caneta azul, azul caneta<h2>")
+    return HttpResponse("<h1>Olha, se voce não me ama.<h1><h2>Caneta azul, azul caneta<h2>")
 
 # Create your views here.
